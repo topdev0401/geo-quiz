fe4a8c16afadbd03f09f280930b0f8ce09bb936d
topdev0401 <helloworld0401@outlook.com>
2019-11-11 13:03:25 +0000

    get correct answer for geo question

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,18 +3,48 @@ import React from 'react';
 class Question extends React.Component {
   constructor(props) {
     super(props);
+
+    this.getRandomCountry = this.getRandomCountry.bind(this);
+  }
+
+  // Generate random country from API
+  getRandomCountry() {
+    return this.props.countries[Math.floor(
+        Math.random() * this.props.countries.length
+    )];
   }
 
   render(){
+    const selectedCountry = this.getRandomCountry();
 
-    // Generate random country from API
-    const randomCountry = this.props.countries[Math.floor(
-        Math.random() * this.props.countries.length
-    )];
+    let correctAnswer;
+    switch (this.props.category) {
+        case "capital":
+            correctAnswer = (
+                <h1>{selectedCountry.capital}</h1>
+            )
+            break;
+        
+        case "population":
+            correctAnswer = (
+                <h1>{selectedCountry.population}</h1>
+            )
+            break;
+        
+        case "flag":
+            correctAnswer = (
+                <img src={selectedCountry.flag}></img>
+            )
+            break;
+        
+        default:
+            correctAnswer = "";
+    }
 
     return(
       <div>
-        <h1>What is the {this.props.category} of {randomCountry.name}?</h1>
+        <h1>What is the {this.props.category} of {selectedCountry.name}?</h1>
+        {correctAnswer}
       </div>
     )
   }
