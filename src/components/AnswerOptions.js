ad0e65f27e1de1e99b73c4ab03cabe7f18100320
topdev0401 <helloworld0401@outlook.com>
2019-12-11 14:24:51 +0000

    display question result on answer option click

--- src/components/AnswerOptions.js
+++ src/components/AnswerOptions.js
@@ -1,41 +1,66 @@
 import React from 'react';
 
 import { getRandomCountry, shuffle } from "../utils";
-import Question from './Question';
 
-const AnswerOptions = ({ correctCountry, category, countries }) => {
+class AnswerOptions extends React.Component {
+    constructor(props) {
+        super(props);
 
-    const answersArray = [];
-    answersArray[0] = getRandomCountry(countries)[category];
-    answersArray[1] = getRandomCountry(countries)[category];
-    answersArray[2] = correctCountry[category];
-    shuffle(answersArray);
+        this.correctCountry = this.props.correctCountry;
+        this.category = this.props.category;
+        this.countries = this.props.countries;
+        this.answersArray = [];
+        this.answersArray[0] = getRandomCountry(this.countries)[this.category];
+        this.answersArray[1] = getRandomCountry(this.countries)[this.category];
+        this.answersArray[2] = this.correctCountry[this.category];
+        shuffle(this.answersArray);
 
-    const checkAnswer = (answer) => {
-        if (answer === correctCountry[category]) {
-            console.log("Correct");
-        } else {
-            console.log("Wrong");
+        this.state = {
+            isCorrect: null
         }
+    }
+
+    checkAnswer = (answer) => {
+        document.getElementsByClassName('result')[0].style.display = "flex";
+        this.setState({
+            isCorrect: answer === this.correctCountry[this.category]
+        });
     };
 
-    const answerButtons = answersArray.map((answer) => {
-        if (category !== "flag") {
-            return (
-                <button key={answer} onClick={() => checkAnswer(answer)}>{answer}</button>
-            )
+    render() {
+        const answerButtons = this.answersArray.map((answer) => {
+            if (this.category !== "flag") {
+                return (
+                    <button key={answer} onClick={() => this.checkAnswer(answer)}>{answer}</button>
+                )
+            } else {
+                return (
+                    <img className="flag" src={answer} alt="flag" key={answer}></img>
+                )
+            }
+        });
+
+        let result = null;
+        if (this.state.isCorrect) {
+            result = (
+                <div className="result result--correct">Correct!</div>
+            );
         } else {
-            return (
-                <img className="flag" src={answer} alt="flag" key={answer}></img>                
+            result = (
+                <div className="result result--incorrect">Wrong!</div>
             )
         }
-    });
 
-    return (
-        <div className="answer-options">
-            {answerButtons}
-        </div>
-    )
+        return (
+            <>
+                <div className="answer-options">
+                    {answerButtons}
+                </div>
+
+                {result}
+            </>
+        )
+    }
 };
 
 export default AnswerOptions;
