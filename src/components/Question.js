4be194985a61e7a045f81e9f80b7ab4d8cecebca
topdev0401 <helloworld0401@outlook.com>
2020-11-10 15:10:59 +0000

    refactor(question): use hooks

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,68 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AnswerOptions from "./AnswerOptions";
 import QuizEnd from './QuizEnd';
-
 import { getRandomItem } from "../utils";
 
-class Question extends React.Component {
-  constructor(props) {
-    super(props);
+const Question = (props) => {
+  let [questionsAnswered, addQuestionAnswered] = useState(0);
+  let [correctAnswers, addCorrectAnswer] = useState(0);
 
-    this.state = {
-      newQuestionGenerated: false,
-      questionsAnswered: 0,
-      correctAnswers: 0
-    }
-
-    this.generateNewQuestion = this.generateNewQuestion.bind(this);
-  }
-
-  generateNewQuestion(prevResult) {
-    let { questionsAnswered, correctAnswers } = this.state;
+  const generateNewQuestion = (prevResult) => {
     questionsAnswered++;
 
     // Add one to the score if the answer was correct
     if (prevResult) correctAnswers++;
 
-    this.setState({
-      newQuestionGenerated: true,
-      questionsAnswered,
-      correctAnswers
-    });
+    addQuestionAnswered(questionsAnswered)
+    addCorrectAnswer(correctAnswers);
   }
 
-  render(){
-    const { categories, countries, number } = this.props;
+  const { categories, countries, number } = props;
 
-    let selectedCountry = getRandomItem(countries);
-    let category = getRandomItem(categories);
+  let selectedCountry = getRandomItem(countries);
+  let category = getRandomItem(categories);
 
-    if (!selectedCountry[category]) {
-      selectedCountry = getRandomItem(countries);
-      category = getRandomItem(categories);
-    };
+  if (!selectedCountry[category]) {
+    selectedCountry = getRandomItem(countries);
+    category = getRandomItem(categories);
+  };
 
-    // Check if the user has answered every question
-    if (this.state.questionsAnswered === number) {
-      return (
-        <QuizEnd score={this.state.correctAnswers} questionsAnswered={this.state.questionsAnswered} />
-      )
-    }
-
-    return(
-      <div className="question-container">
-        <p>{this.state.questionsAnswered + 1}/{number}</p>
-        <h1 className="title">What is the {category} of {selectedCountry.name}?</h1>
-        <AnswerOptions
-          correctCountry={selectedCountry}
-          category={category}
-          countries={countries}
-          generateNewQuestion={this.generateNewQuestion}
-        />
-      </div>
+  // Check if the user has answered every question
+  if (questionsAnswered === number) {
+    return (
+      <QuizEnd score={correctAnswers} questionsAnswered={questionsAnswered} />
     )
   }
+
+  return (
+    <div className="question-container">
+      <p>{questionsAnswered + 1}/{number}</p>
+      <h1 className="title">What is the {category} of {selectedCountry.name}?</h1>
+
+      <AnswerOptions
+        correctCountry={selectedCountry}
+        category={category}
+        countries={countries}
+        generateNewQuestion={generateNewQuestion}
+      />
+    </div>
+  )
 }
 
 export default Question;
