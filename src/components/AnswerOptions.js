4282c4c87cbb1fc5064fe3855b08a0b72cb9412f
topdev0401 <helloworld0401@outlook.com>
2021-12-08 19:33:52 +0000

    props destructuring on answer options

--- src/components/AnswerOptions.js
+++ src/components/AnswerOptions.js
@@ -1,94 +1,93 @@
 import React, { useState, useEffect } from 'react';
 
 import {
-    getRandomItem,
-    shuffle,
-    hasDuplicates,
-    hasEmptyValue
+  getRandomItem,
+  shuffle,
+  hasDuplicates,
+  hasEmptyValue
 } from "../utils";
 
-const AnswerOptions = (props) => {
-    const [isCorrect, toggleIsCorrect] = useState(null);
-    const [buttonsDisabled, toggleButton] = useState(false);
-    const [answers, setAnswersArray] = useState([])
+const AnswerOptions = ({ correctCountry, category, countries, generateNewQuestion }) => {
+  const [isCorrect, toggleIsCorrect] = useState(null);
+  const [buttonsDisabled, toggleButton] = useState(false);
+  const [answers, setAnswersArray] = useState([]);
 
-    useEffect(() => {
-        generateAnswers();
+  useEffect(() => {
+    generateAnswers();
 
-        toggleIsCorrect(null);
-        toggleButton(false);
+    toggleIsCorrect(null);
+    toggleButton(false);
 
-        // Hide the result from the previous question
-        document.getElementsByClassName('result')[0].style.display = "none";
-    }, [props]);
+    // Hide the result from the previous question
+    document.getElementsByClassName('result')[0].style.display = "none";
+  }, [correctCountry, category, countries]);
 
-    const generateAnswers = () => {
-        const { correctCountry, category, countries } = props;
+  const generateAnswers = () => {
 
-        let answersArray = [];
-        answersArray[0] = getRandomItem(countries)[category];
-        answersArray[1] = getRandomItem(countries)[category];
-        answersArray[2] = correctCountry[category];
+    let answersArray = [];
+    answersArray[0] = getRandomItem(countries)[category];
+    answersArray[1] = getRandomItem(countries)[category];
+    answersArray[2] = correctCountry[category];
 
-        setAnswersArray(answersArray);
+    setAnswersArray(answersArray);
 
-        // Regenerate the array if there are duplicates or it contains an empty value
-        if (hasDuplicates(answersArray) || hasEmptyValue(answersArray)) {
-            generateAnswers();
-        }
-
-        // Randomise the order in which the answers apear
-        shuffle(answersArray);
+    // Regenerate the array if there are duplicates or it contains an empty value
+    if (hasDuplicates(answersArray) || hasEmptyValue(answersArray)) {
+      generateAnswers();
     }
 
-    const checkAnswer = (answer) => {
-        document.getElementsByClassName('result')[0].style.display = "flex";
-        toggleButton(true);
-        toggleIsCorrect(answer === props.correctCountry[props.category])
-    };
+    // Randomise the order in which the answers apear
+    shuffle(answersArray);
+  }
 
-    const answerButtons = answers.map((answer) => {
-        if (props.category !== "flag") {
-            return (
-                <button disabled={buttonsDisabled} className="answer-option text-button" key={answer} onClick={() => checkAnswer(answer)}>
-                    {answer.toLocaleString()}
-                </button>
-            )
-        } else {
-            return (
-                <button disabled={buttonsDisabled} type="button" onClick={() => checkAnswer(answer)} className="answer-option" key={answer}>
-                    <img className="flag" src={answer} alt="flag"></img>
-                </button>
-            )
-        }
-    });
+  const checkAnswer = (answer) => {
+    document.getElementsByClassName('result')[0].style.display = "flex";
+    toggleButton(true);
+    toggleIsCorrect(answer === correctCountry[category])
+  };
 
-    let result = null;
-    if (isCorrect) {
-        result = <div className="result result--correct">Correct!</div>
+  const answerButtons = answers.map((answer) => {
+    if (category !== "flag") {
+      return (
+        <button disabled={buttonsDisabled} className="answer-option text-button" key={answer} onClick={() => checkAnswer(answer)}>
+          {answer.toLocaleString()}
+        </button>
+      )
     } else {
-        result = <div className="result result--incorrect">Wrong!</div>
+      return (
+        <button disabled={buttonsDisabled} type="button" onClick={() => checkAnswer(answer)} className="answer-option" key={answer}>
+          <img className="flag" src={answer} alt="flag"></img>
+        </button>
+      )
     }
+  });
 
-    let nextButton = null;
-    if (isCorrect !== null) {
-        nextButton = (
-            <button className="restart-button" onClick={() => { props.generateNewQuestion(isCorrect) }}>
-                Next
-            </button>
-        );
-    }
+  let result = null;
+  if (isCorrect) {
+    result = <div className="result result--correct">Correct!</div>
+  } else {
+    result = <div className="result result--incorrect">Wrong!</div>
+  }
 
-    return (
-        <>
-            <div className="answer-options">
-                {answerButtons}
-            </div>
+  let nextButton = null;
+  if (isCorrect !== null) {
+    nextButton = (
+      <button className="restart-button" onClick={() => { generateNewQuestion(isCorrect) }}>
+        Next
+      </button>
+    );
+  }
 
-            {result}
-            {nextButton}
-        </>
-    )
+  return (
+    <>
+      <div className="answer-options">
+        {answerButtons}
+      </div>
+
+      {result}
+      {nextButton}
+    </>
+  )
 };
 
 export default AnswerOptions;
